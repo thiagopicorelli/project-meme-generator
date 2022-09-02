0048a7200b5409ada372b6688aecea8c2753fc6b
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 14:45:38 -0300

    Adiciona carregamento de imagem no container

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const inputTexto = document.getElementById('text-input');
+const inputImagem = document.getElementById('meme-insert')
 
 const memeTexto = document.getElementById('meme-text');
+const memeImagem = document.getElementById('meme-image');
 
 function atualizaTexto() {
   memeTexto.innerHTML = inputTexto.value;
 }
 
 inputTexto.addEventListener('input', atualizaTexto);
+
+function atualizaImagem(event) {
+  let fr = new FileReader();
+
+  fr.addEventListener('load', () => {
+    memeImagem.src = fr.result;
+  });
+
+  fr.readAsDataURL(event.target.files[0]);
+}
+
+inputImagem.addEventListener('change', atualizaImagem);
