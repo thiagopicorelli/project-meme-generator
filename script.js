bd179b1eff161fd5d1ee027542c8ec76de771cce
thiagopicorelli <thiagopicorellichalet@gmail.com>
2022-09-02 15:22:17 -0300

    Adiciona modificação da borda do container

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const inputTexto = document.getElementById('text-input');
-const inputImagem = document.getElementById('meme-insert')
+const inputImagem = document.getElementById('meme-insert');
 
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+
+const memeContainer = document.getElementById('meme-image-container');
 const memeTexto = document.getElementById('meme-text');
 const memeImagem = document.getElementById('meme-image');
 
@@ -21,3 +26,17 @@ function atualizaImagem(event) {
 }
 
 inputImagem.addEventListener('change', atualizaImagem);
+
+let bordas = {
+  fire: '3px dashed red',
+  water: '5px double blue',
+  earth: '6px groove green',
+};
+
+function atualizaBorda(tipo) {
+  memeContainer.style.border = bordas[tipo];
+}
+
+fireButton.addEventListener('click', () => {atualizaBorda('fire');});
+waterButton.addEventListener('click', () => {atualizaBorda('water');});
+earthButton.addEventListener('click', () => {atualizaBorda('earth');});
